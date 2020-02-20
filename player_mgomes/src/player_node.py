84b250eb2fbd2f9025e630043c8cbe1aa78fd661
Manuel Gomes <gomesnelito@gmail.com>
2020-02-20 17:28:02 +0100

    Now we have velocity and angles!

--- player_mgomes/src/player_node.py
+++ player_mgomes/src/player_node.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 import math
-
+import random
 import rospy
+import numpy
+import tf
 from std_msgs.msg import String
 from rws2020_msgs.msg import MakeAPlay
+from geometry_msgs.msg import Transform, Quaternion
 
 
 class Player:
@@ -35,7 +38,64 @@ class Player:
             'I am ' + self.player_name + ' and team ' + self.my_team + ' is the greatest. Team ' + self.prey_team + ' are all going to die! Be careful with ' + self.hunter_team + ' team.')
 
         rospy.Subscriber("make_a_play", MakeAPlay, self.makeAPlayCallback)
-        # self.br
+        self.br = tf.TransformBroadcaster()
+        self.transform = Transform()
+        Initial_R = 8 * random.random()
+        Initial_Theta = 2 * math.pi * random.random()
+        Initial_X = Initial_R * math.cos(Initial_Theta)
+        Initial_Y = Initial_R * math.sin(Initial_Theta)
+        Initial_Rotation = 2 * math.pi * random.random()
+        self.transform.translation.x = Initial_X
+        self.transform.translation.y = Initial_Y
+        self.transform.rotation= tf.transformations.quaternion_from_euler(0, 0, Initial_Rotation)
+
+    def move(self, transform_now, vel, angle):
+
+        if angle > self.max_angle:
+            angle = self.max_angle
+        elif angle < -self.max_angle:
+            angle = -self.max_angle
+
+        if vel > self.max_vel:
+            vel = self.max_vel
+
+        T1 = transform_now
+
+        T2 = Transform()
+        T2.rotation = tf.transformations.quaternion_from_euler(0, 0, angle)
+        T2.translation.x = vel
+        matrix_trans = tf.transformations.translation_matrix((T2.translation.x,
+                                                              T2.translation.y,
+                                                              T2.translation.z))
+
+        matrix_rot = tf.transformations.quaternion_matrix((T2.rotation[0],
+                                                           T2.rotation[1],
+                                                           T2.rotation[2],
+                                                           T2.rotation[3]))
+        matrixT2 = numpy.matmul(matrix_trans, matrix_rot)
+
+        matrix_trans = tf.transformations.translation_matrix((T1.translation.x,
+                                                              T1.translation.y,
+                                                              T1.translation.z))
+
+        matrix_rot = tf.transformations.quaternion_matrix((T1.rotation.x,
+                                                           T1.rotation.y,
+                                                           T1.rotation.z,
+                                                           T1.rotation.w))
+        matrixT1 = numpy.matmul(matrix_trans, matrix_rot)
+
+        matrix_new_transform = numpy.matmul(matrixT2, matrixT1)
+
+        quat = tf.transformations.quaternion_from_matrix(matrix_new_transform)
+        trans = tf.transformations.translation_from_matrix(matrix_new_transform)
+
+        self.transform.rotation = Quaternion(quat[0], quat[1], quat[2], quat[3])
+        self.transform.translation.x = trans[0]
+        self.transform.translation.y = trans[1]
+        self.transform.translation.z = trans[0]
+
+        self.br.sendTransform(trans, quat, rospy.Time.now(),
+                              self.player_name, "world")
 
     def makeAPlayCallback(self, msg):
         self.max_vel = msg.turtle
@@ -44,6 +104,15 @@ class Player:
         vel = self.max_vel
         angle = self.max_angle
         # self.br.sendTransform( (0,0,0))
+        TDeslocamento = Transform()
+        TDeslocamento.rotation = tf.transformation.quaternion_from_euler(0,0,angle)
+        TDeslocamento.translation.x = vel
+
+
+
+
+
+
 
 
 def callback(msg):
